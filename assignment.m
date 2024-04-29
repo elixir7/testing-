ed29a3334d9686e557734ef0d6916f05cf912f4f
Isak Åslund <aslundisak@gmail.com>
2024-04-29 21:48:20 +0200

    Initial commit

---
+++ assignment.m
@@ -0,0 +1,36 @@
+clc; clf; clear all
+
+sigma = @(x) x >= 0;
+g = @(t) cos(t) .* (sigma(t + pi/2) - sigma(t - pi/2));
+
+T = pi;
+t = -15:0.001:15;
+x = zeros(1, length(t)); 
+
+for n = -3:3
+    x(1,:) = x(1,:) + g(t + n*T);
+end
+
+plot(t, x);
+grid on;
+title("$$x(t) = \sum_{n=-3}^3g(t + nT)$$", "Interpreter", "latex");
+xlabel("t"); ylabel("x(t)");
+
+
+%%
+i = randi([-10,10], 1, 10e6);
+
+[big_val, big_index] = big(i)
+
+function [biggest, index] = big(x)
+    biggest = -Inf;
+    for i = 1:length(x)
+        if x(i) > biggest
+           biggest = x(i); 
+           index = i;
+        end
+    end
+end
+
+
+
